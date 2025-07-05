6ae9a0fd966c2f8a11661cbb3e10f013144305e7
hairyfred <30961255+hairyfred@users.noreply.github.com>
2025-07-05 15:58:54 +0100

    Initial Foundations

---
+++ PSGoodtop.ps1
@@ -0,0 +1,128 @@
+# Parameters
+param(
+    # Switches for differnt functions
+    [Parameter(ParameterSetName = 'port')]
+    [switch]$port,
+    [Parameter(ParameterSetName = 'poe')]
+    [switch]$poe,
+    # Global Information
+    [Parameter(Mandatory)] 
+    [string]$username = "admin",
+    [Parameter(Mandatory)] 
+    [string]$password = "admin",
+    [Parameter(Mandatory)] 
+    [string]$url,
+    [Parameter()]
+    [int]$interface='-1',
+    [Parameter()]
+    [int]$enabled='-1',
+    # Port Settings Information
+    [Parameter(Mandatory, ParameterSetName = 'port')]
+    [string]$speed,
+    [Parameter(Mandatory, ParameterSetName = 'port')]
+    [int]$flow
+)
+# Info needed for each page
+$pages = [pscustomobject]@{
+    info = @{
+        url = "info.cgi"
+    }
+    port = @{
+        url = "port.cgi"
+        body = @{
+            submit = "+++Apply+++"
+            cmd = "port"
+        }
+    }
+    poe = @{
+        url = "pse_port.cgi"
+        body = @{
+            language = "EN"
+            submit = "apply"
+            cmd = "poe"
+        }
+    }
+}
+# Adds param data
+switch ($PSCmdlet.ParameterSetName) {
+    'port' {
+        if ($interface -eq -1) {
+            $interface = Read-Host "Interface Number"
+        }
+        if ($enabled -eq -1)  {
+            $enabled = Read-Host "Enabled"
+        }
+        $speedMap = @{
+            "auto"     = 0
+            "10HD"     = 1
+            "10FD"     = 2
+            "100HD"    = 3
+            "100FD"    = 4
+            "1000FD"   = 5
+            "2500FD"   = 6
+            "5000FD"   = 7  # May be unsupported on your switch
+            "10000FD"  = 8
+        }
+        if ($speedMap.ContainsKey($speed)) {
+            $speed = $speedMap[$speed]
+        }
+        $pages.port.body.speed_duplex = "$speed"
+        $pages.port.body.flow = $flow
+        $pages.port.body.state = $enabled
+        $pages.port.body.portid = $interface
+        $selectedbody = $pages.port.body
+        $selectedpage = $pages.port.url
+    }
+    'poe' {
+        if ($interface -eq -1) {
+            $interface = Read-Host "Interface Number:"
+        }
+        if ($enabled -eq -1)  {
+            $enabled = Read-Host "Enabled"
+        }
+        $pages.poe.body.state = $enabled
+        $pages.poe.body.portid = $interface
+        $selectedbody = $pages.poe.body
+        $selectedpage = $pages.poe.url
+    }
+}
+
+function Get-LoginCookie {
+    # Get Login Cookie
+    # Combine username and password
+    $logincombo = "$username$password"
+
+    # Hash with MD5
+    $md5 = [System.Security.Cryptography.MD5]::Create()
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($logincombo)
+    $hashBytes = $md5.ComputeHash($bytes)
+    $hashedlogin = (-join ($hashBytes | ForEach-Object { $_.ToString("x2") }))
+    return "$hashedlogin"
+}
+
+
+# Headers, some of these are likley not needed and will trim out later
+$headers = @{
+    "Host" = "$url"
+    "Connection" = "keep-alive"
+    "Cache-Control" = "max-age=0"
+    "Upgrade-Insecure-Requests" = "1"
+    "Origin" = "http://$url"
+    "Content-Type" = "application/x-www-form-urlencoded"
+    "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
+    "Referer" = "http://$url/$page"
+    "Accept-Encoding" = "gzip, deflate"
+}
+# Add Login Cookie
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$cookie = New-Object System.Net.Cookie("admin", (Get-LoginCookie), "/", "$url")
+$session.Cookies.Add($cookie)
+
+# Run the command
+$response = Invoke-RestMethod -Uri "$url/$selectedpage" `
+    -Method POST `
+    -Body $selectedbody `
+    -WebSession $session `
+    -Headers $headers `
+    -ContentType "application/x-www-form-urlencoded" `
+    -UseBasicParsing
---
+++ README.md
@@ -0,0 +1,36 @@
+# Goodtop-PowerShell
+A powershell script to allow you to control features/ports of a Goodtop switch
+
+WARNING: My code should not be counted as relabile, this should not be anywhere near being considered to use in production
+
+# Features
+* Enable/Disable Ports
+* Change Speed/Duplex and Flow
+* Toggle Port PoE
+
+# Usage
+Required Global Paramters
+| Parameter   | Description                                                       |
+| ----------- | ------------------------------------------------------------------|
+| `-username` | Login username (default: `admin`)                                 |
+| `-password` | Login password (default: `admin`)                                 |
+| `-url`      | The base domain or IP address of the switch (e.g., `192.168.1.1`) |
+
+Port Settings
+| Parameter    | Description                                                                            |
+| ------------ | -------------------------------------------------------------------------------------- |
+| `-port`      | **Required**: Selects port configuration mode                                          |
+| `-speed`     | Port speed and duplex (e.g. `1000FD`, `100HD`, etc.)                                   |
+| `-flow`      | Flow control state (`0` or `1`)                                                        |
+| `-interface` | Port/interface number                                                                  |
+| `-enabled`   | Port state: `0` (disabled), `1` (enabled)                                              |
+
+PoE settings
+| Parameter    | Description                                                               |
+| ------------ | ------------------------------------------------------------------------- |
+| `-poe`       | **Required**: Selects PoE configuration mode                              |
+| `-interface` | Port/interface number                                                     |
+| `-enabled`   | PoE state: `0` (off), `1` (on)                                            |
+
+# Tested Models
+ZX-AFGW-SWTG218ANS-100
